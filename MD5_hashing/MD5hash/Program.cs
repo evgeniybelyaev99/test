87dad38bbb1c0e5acb83bfae533bb874f558916b
Evgeniy Belyaev <evgeniy_belyaev_1999@mail.ru>
2021-02-02 22:28:02 +0300

    test

---
+++ MD5_hashing/MD5hash/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MD5hash
+{
+    public class Program
+    {
+         static int Main(string[] args)
+         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Входной аргумент не указан!");
+                return 0;
+            }
+            string message = args[0];
+            Console.WriteLine("Input: {0}", message);
+            Console.WriteLine();
+            string output = Algorithm.HashText(message);
+            Console.WriteLine("MD5 (\"{0}\") = {1}", message, output);
+            return 0;
+         }
+    }
+}
